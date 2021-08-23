45bf4411e75ed2f855a3535324fc988c40b1e071
APrzysiuda <180517@umons.ac.be>
2021-08-23 16:13:21 +0200

    refactor: drop link with s3 bucket

---
+++ flywayjar/src/main/java/flywayjar/Main.java
@@ -0,0 +1,120 @@
+package flywayjar;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.AmazonClientException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.flywaydb.core.Flyway;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+//attention on va voir si j'ai bon !!!
+class s3client extends AmazonS3Client{
+    //S3client
+    public s3client(){
+        AmazonS3ClientBuilder.standard().build();
+    }
+
+    //listing object in bucket
+    public List<S3ObjectSummary> getBucketObjectSummaries(String bucketName){
+        List<S3ObjectSummary> s3ObjectSummaries=new ArrayList<S3ObjectSummary>();
+        try {
+            ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucketName);
+            ObjectListing objectListing;
+
+            do {
+                objectListing = this.listObjects(listObjectsRequest);
+                for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                    s3ObjectSummaries.add(objectSummary);
+                }
+                listObjectsRequest.setMarker(objectListing.getNextMarker());
+            } while (objectListing.isTruncated());
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, " +
+                    "which means your request made it " +
+                    "to Amazon BdS3Client, but was rejected with an error response " +
+                    "for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, " +
+                    "which means the client encountered " +
+                    "an internal error while trying to communicate" +
+                    " with BdS3Client, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+        return s3ObjectSummaries;
+    }
+
+
+    public List<String> getBucketObjectNames(String bucketName){
+        List<String> s3ObjectNames = new ArrayList<String>();
+        List<S3ObjectSummary> s3ObjectSummaries = getBucketObjectSummaries(bucketName);
+
+        for(S3ObjectSummary s3ObjectSummary : s3ObjectSummaries){
+            s3ObjectNames.add(s3ObjectSummary.getKey());
+        }
+        return s3ObjectNames;
+    }
+
+}
+
+public class Main {
+    public String handleRequest (Map<String, Object> input, Context context) throws IOException {
+        //parameter
+        String region= System.getenv("region");
+        String bucketName= System.getenv("bucketName");
+
+        //login ID (name, password and url) (depend of the db and the type of db)
+        /*String secret=System.getenv("secret");
+
+        JsonObject jsonsecret = new Gson().fromJson(secret,JsonObject.class);
+        String username= jsonsecret.get("username").toString().replace('"',' ').strip();
+        String password=jsonsecret.get("password").toString().replace('"',' ').strip();
+
+        String host=jsonsecret.get("host").toString().replace('"',' ').strip();
+        String port=jsonsecret.get("port").toString().replace('"',' ').strip();
+        String dbname=jsonsecret.get("dbname").toString().replace('"',' ').strip();
+        String url="jdbc:redshift://"+host+":"+port+"/"+dbname;*/
+        String username=System.getenv("user");
+        String url=System.getenv("url");
+        String password=System.getenv("password");
+
+        //path for files (always tmp for flyway)
+        Path outputPath= Paths.get("/tmp");
+        System.out.println("initialisation du client");
+        //s3client
+        s3client client =new s3client();
+        System.out.println("Client créer");
+        List<String> objectList = client.getBucketObjectNames(bucketName);
+        System.out.println("entrer dans la boucle");
+        //save object in tmp
+        for(String objectName: objectList){
+            GetObjectRequest getObjectRequest=new GetObjectRequest(bucketName,objectName);
+            String paths="/tmp/"+objectName;
+            client.getObject(getObjectRequest,new File(paths));
+        }
+        System.out.println("initialisation de la migration");
+        //configure flyway with
+        Flyway flyway = Flyway.configure().dataSource(url, username, password).locations("filesystem:/tmp/").load();
+        // Start the migration
+        flyway.migrate();
+
+        return "Migrate successfull";
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as lambda from '@aws-cdk/aws-lambda';
+import * as awsLambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 
 export class FlywayConstruct extends cdk.Construct {
-  flywayLambdaMigration: lambda.Function;
+  flywayLambdaMigration: awsLambda.Function;
 
   constructor(scope: cdk.Construct,
     id: string,
@@ -16,7 +16,7 @@ export class FlywayConstruct extends cdk.Construct {
     user: string,
     password: string) {
     super(scope, id);
-    this.flywayLambdaMigration = new lambda.Function(this, 'flywayLambdaMigration', {
+    this.flywayLambdaMigration = new awsLambda.Function(this, 'flywayLambdaMigration', {
       vpc: VPC,
       vpcSubnets: subnet,
       securityGroups: [securityGroup],
@@ -24,15 +24,14 @@ export class FlywayConstruct extends cdk.Construct {
       memorySize: 512,
       timeout: cdk.Duration.seconds(30),
       handler: 'flywayjar.Main::handleRequest',
-      runtime: lambda.Runtime.JAVA_11,
+      runtime: awsLambda.Runtime.JAVA_11,
       environment: {
         url: url,
         bucketName: bucketName,
         user: user,
         password: password,
       },
-      code: lambda.S3Code.fromBucket(s3.Bucket.fromBucketArn(this, 'zipBucket', 'arn:aws:s3:::flywaymigrationconstruct'),
-        'flywayjar.zip'),
+      code: awsLambda.Code.fromAsset('flywayjar.zip'),
     });
   }
 }
