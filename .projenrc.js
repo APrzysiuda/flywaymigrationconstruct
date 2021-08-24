6cb9a3c74bda941ed6845d25b22deede515315c9
APrzysiuda <180517@umons.ac.be>
2021-08-24 12:01:50 +0200

    fix: fix upload wrong gradle version

--- .projenrc.js
+++ .projenrc.js
@@ -71,7 +71,7 @@ task1.addJobs({
         uses: 'gradle/wrapper-validation-action@e6e38bacfdf1a337459f332974bb2327a31aaf4b',
       },
       {
-        run: 'gradle build && gradle buildZip',
+        run: 'gradle wrapper --gradle-version 6.7 && gradle build && gradle buildZip',
       },
       {
         name: 'run upload !',
