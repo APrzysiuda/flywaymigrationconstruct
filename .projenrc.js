0d31385952b6648e639285b10628b153b8cf6143
APrzysiuda <180517@umons.ac.be>
2021-08-25 14:01:26 +0200

    fix: use temporary file for upload

--- .projenrc.js
+++ .projenrc.js
@@ -65,8 +65,11 @@ task1.addJobs({
         'working-directory': './flywayjar',
       },
       {
+        uses: 'actions/upload-artifact@v1',
+      },
+      {
         name: 'run upload !',
-        run: 'mkdir ./temp && cp ./bin/ ./temp/flywayjar.zip && aws s3 sync ./temp/ s3://flywaymigrationconstruct',
+        run: 'mkdir ./temp && cp ./flywayjar/build/distributions/flywayjar-1.0-SNAPSHOT.zip ./temp/flywayjar.zip && aws s3 sync ./temp/ s3://flywaymigrationconstruct',
         env: {
           AWS_ACCESS_KEY_ID: '${{secrets.AWS_ACCESS_KEY_ID}}',
           AWS_SECRET_ACCESS_KEY: '${{secrets.AWS_SECRET_ACCESS_KEY}}',
