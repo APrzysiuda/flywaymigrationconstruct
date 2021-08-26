45589f1d5f58e2dcc58abefb3e279d0157efbd5c
APrzysiuda <180517@umons.ac.be>
2021-08-26 10:29:23 +0200

    fix: jar on git

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,5 +1,9 @@
-import { Hello } from '../src';
+import { countResources, expect as expectCDK } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
 
-test('hello', () => {
-  expect(new Hello().sayHello()).toBe('hello, world!');
+test('Dumb test', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'TestStack');
+
+  expectCDK(stack).to(countResources('AWS::Lambda::Function', 0));
 });
