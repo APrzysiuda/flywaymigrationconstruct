4cc77068bfe52e43fc826344fe3efb45ede16600
APrzysiuda <180517@umons.ac.be>
2021-09-30 17:33:55 +0200

    fix: vpc condition

--- src/index.ts
+++ src/index.ts
@@ -4,62 +4,64 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as awssecret from '@aws-cdk/aws-secretsmanager';
 import * as cdk from '@aws-cdk/core';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const pjson= require('../package.json');
+const pjson = require('../package.json');
 
 
-export interface VPCParams{
-  readonly vpc: ec2.IVpc;
-  readonly subnet: ec2.SubnetSelection;
-  readonly securityGroups: ec2.ISecurityGroup[];
+export interface VPCParams {
+    readonly vpc: ec2.IVpc;
+    readonly subnet: ec2.SubnetSelection;
+    readonly securityGroups: ec2.ISecurityGroup[];
 }
+
 export interface FlywayConstructParams {
-  readonly migrationDBSecretManager: awssecret.ISecret;
-  readonly bucketMigrationSQL: s3.IBucket;
-  readonly vpc?: ec2.IVpc;
-  readonly subnet?: ec2.SubnetSelection;
-  readonly securityGroups?: ec2.ISecurityGroup[];
-  readonly memorySize?: number;
-  readonly timeout?: cdk.Duration;
-  //add readonly bucketFolderPrefix? : string;
+    readonly migrationDBSecretManager: awssecret.ISecret;
+    readonly bucketMigrationSQL: s3.IBucket;
+    readonly vpc?: ec2.IVpc;
+    readonly subnet?: ec2.SubnetSelection;
+    readonly securityGroups?: ec2.ISecurityGroup[];
+    readonly memorySize?: number;
+    readonly timeout?: cdk.Duration;
+    //add readonly bucketFolderPrefix? : string;
 }
+
 export class FlywayConstruct extends cdk.Construct {
 
-  static readonly HANDLER = 'tech.necko.flywayjar.Main::handleRequest';
-  static readonly ID_LAMBDA_CODE = 'bucketMigration';
-  static readonly BUCKET_CODE_ARN = 'arn:aws:s3:::flywaymigrationconstruct';
-  objectCodeKey = pjson.version;
-  flywayLambdaMigration: awsLambda.Function;
+    static readonly HANDLER = 'tech.necko.flywayjar.Main::handleRequest';
+    static readonly ID_LAMBDA_CODE = 'bucketMigration';
+    static readonly BUCKET_CODE_ARN = 'arn:aws:s3:::flywaymigrationconstruct';
+    objectCodeKey = pjson.version;
+    flywayLambdaMigration: awsLambda.Function;
 
-  constructor(scope: cdk.Construct,
-    id: string,
-    params: FlywayConstructParams,
-  ) {
-    super(scope, id);
+    constructor(scope: cdk.Construct,
+                id: string,
+                params: FlywayConstructParams,
+    ) {
+        super(scope, id);
 
 
-    if ((params.subnet || params.securityGroups) && !params.vpc){
-      throw new Error('you cant pass subnet or securitygroup without vpc');
+        if ((params.subnet || params.securityGroups) && !params.vpc) {
+            throw new Error('you cant pass subnet or securitygroup without vpc');
+        }
+        if ((!params.subnet || !params.securityGroups) && params.vpc) {
+            throw new Error('you cant pass vpc without subnet and securityGroups');
+        }
+        this.flywayLambdaMigration = new awsLambda.Function(this, id, {
+            vpc: params.vpc,
+            vpcSubnets: params.subnet,
+            securityGroups: params.securityGroups,
+            memorySize: params.memorySize || 512,
+            timeout: params.timeout || cdk.Duration.seconds(30),
+            handler: FlywayConstruct.HANDLER,
+            runtime: awsLambda.Runtime.JAVA_11,
+            environment: {
+                //add PREFIX: params.bucketFolderPrefix || ''
+                ARN: params.migrationDBSecretManager.secretArn,
+                BUCKET_NAME: params.bucketMigrationSQL.bucketName,
+            },
+            code: awsLambda.S3Code.fromBucket(
+                s3.Bucket.fromBucketArn(this, FlywayConstruct.ID_LAMBDA_CODE, FlywayConstruct.BUCKET_CODE_ARN), 'flywayjar.' + this.objectCodeKey + '.zip'),
+        });
+        params.migrationDBSecretManager.grantRead(this.flywayLambdaMigration);
+        params.bucketMigrationSQL.grantRead(this.flywayLambdaMigration);
     }
-    if ((!params.subnet || !params.securityGroups) && params.vpc) {
-      throw new Error('you cant pass vpc without subnet and securityGroups');
-    }
-    this.flywayLambdaMigration = new awsLambda.Function(this, id, {
-      vpc: params.vpc,
-      vpcSubnets: params.subnet,
-      securityGroups: params.securityGroups,
-      memorySize: params.memorySize || 512,
-      timeout: params.timeout || cdk.Duration.seconds(30),
-      handler: FlywayConstruct.HANDLER,
-      runtime: awsLambda.Runtime.JAVA_11,
-      environment: {
-        //add PREFIX: params.bucketFolderPrefix || ''
-        ARN: params.migrationDBSecretManager.secretArn,
-        BUCKET_NAME: params.bucketMigrationSQL.bucketName,
-      },
-      code: awsLambda.S3Code.fromBucket(
-        s3.Bucket.fromBucketArn(this, FlywayConstruct.ID_LAMBDA_CODE, FlywayConstruct.BUCKET_CODE_ARN), 'flywayjar.'+this.objectCodeKey+'.zip'),
-    });
-    params.migrationDBSecretManager.grantRead(this.flywayLambdaMigration);
-    params.bucketMigrationSQL.grantRead(this.flywayLambdaMigration);
-  }
 }
