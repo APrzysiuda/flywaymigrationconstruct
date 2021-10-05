1e65a36fea8dca9fee0e331a40e21fdb5ce2e2b2
APrzysiuda <180517@umons.ac.be>
2021-10-05 16:57:19 +0200

    Fix: error

--- flywayjar/src/main/java/tech/necko/flywayjar/Main.java
+++ flywayjar/src/main/java/tech/necko/flywayjar/Main.java
@@ -27,7 +27,7 @@ public class Main {
         //parameter
         System.out.println("entrez dans mon handler");
         String bucketName = System.getenv("BUCKET_NAME");
-        String mehdi = "ca marche poas osecour";
+        String mehdi = "ca marche poas osecounbr";
         String arn = System.getenv("ARN");
         //add string prefix = System.getenv("PREFIX");
 
