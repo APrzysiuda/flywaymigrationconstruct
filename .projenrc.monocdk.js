ad909e5a7bc788128930b6d446dfcade4f21d32d
APrzysiuda <180517@umons.ac.be>
2021-10-05 15:31:55 +0200

    Feat: add git workflow for monocdk

--- .projenrc.monocdk.js
+++ .projenrc.monocdk.js
@@ -1,5 +1,4 @@
 const { AwsCdkConstructLibrary, ProjectType, github, git, IgnoreFile } = require('projen');
-const { workflows, GitHub, GithubWorkflow } = require('projen/lib/github');
 const { Task } = require('projen/lib/tasks');
 const project = new AwsCdkConstructLibrary({
   author: 'APrzysiuda',
