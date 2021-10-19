56bcdac31b988168372a9ed83ae86cf351a28f78
APrzysiuda <180517@umons.ac.be>
2021-10-19 12:14:12 +0200

    Fix: Test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -5,14 +5,14 @@ import * as cdk from '@aws-cdk/core';
 import {FlywayConstruct} from '../src';
 
 test('test stack', () => {
-const app = new cdk.App();
-const stack = new cdk.Stack(app, 'Test');
-const bucket = new s3.Bucket(stack, 'bucket');
-const secret = new secrets.Secret(stack, 'secret');
-const construct = new FlywayConstruct(stack, 'testConstruct', {
-    migrationDBSecretManager: secret,
-    bucketMigrationSQL: bucket,
-});
-construct.flywayLambdaMigration;
-expect(stack).to(haveResource('AWS::Lambda::Function'));
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'Test');
+  const bucket = new s3.Bucket(stack, 'bucket');
+  const secret = new secrets.Secret(stack, 'secret');
+  const construct = new FlywayConstruct(stack, 'testConstruct', {
+      migrationDBSecretManager: secret,
+      bucketMigrationSQL: bucket,
+  });
+  construct.flywayLambdaMigration;
+  expect(stack).to(haveResource('AWS::Lambda::Function'));
 });
