0cd8d62f91ea5572dff59ff6e90c81a31e7c1cc0
APrzysiuda <180517@umons.ac.be>
2021-10-19 11:53:47 +0200

    Fix: Test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,18 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as secrets from '@aws-cdk/aws-secretsmanager';
-import {FlywayConstruct} from '../src';
-import {expect, haveResource} from '@aws-cdk/assert';
+import { FlywayConstruct } from '../src';
+import { expect, haveResource } from '@aws-cdk/assert';
 
 test('test stack', () => {
-  const app = new cdk.App();
-  const stack = new cdk.Stack(app, 'Test');
-  const bucket = new s3.Bucket(stack, 'bucket');
-  const secret = new secrets.Secret(stack, 'secret');
-  const construct = new FlywayConstruct(stack, 'testConstruct', {
-    migrationDBSecretManager: secret,
-    bucketMigrationSQL: bucket,
-  });
-  construct.flywayLambdaMigration;
-  expect(stack).to(haveResource('AWS::Lambda::Function'));
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Test');
+    const bucket = new s3.Bucket(stack, 'bucket');
+    const secret = new secrets.Secret(stack, 'secret');
+    const construct = new FlywayConstruct(stack, 'testConstruct', {
+        migrationDBSecretManager: secret,
+        bucketMigrationSQL: bucket,
+    });
+    construct.flywayLambdaMigration;
+    expect(stack).to(haveResource('AWS::Lambda::Function'));
 });
